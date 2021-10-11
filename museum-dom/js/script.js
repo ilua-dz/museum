53e325df786be23de5514628f78d77bb86ec76a5
Ilya Dzyuin <ilua.dzuin@gmail.com>
2021-10-11 19:15:23 +0400

    init: add museum-dom landing

--- museum-dom/js/script.js
+++ museum-dom/js/script.js
@@ -39,8 +39,9 @@ document.addEventListener('DOMContentLoaded', () => setOverlayBodySize());
 
 //*! welcome-slider
 
-const swiper = new Swiper('.swiper', {
+const welcomeSlider = new Swiper('.welcome-slider', {
 	direction: 'horizontal',
+
 	loop: true,
 	effect: 'slide',
 
@@ -56,10 +57,10 @@ const swiper = new Swiper('.swiper', {
 },
 );
 
-document.getElementById("slide-number").textContent = '0' + swiper.activeIndex;
+document.getElementById("slide-number").textContent = '0' + welcomeSlider.activeIndex;
 
-swiper.on('slideChange', function () {
-	document.getElementById("slide-number").textContent = `0${swiper.realIndex + 1}`;
+welcomeSlider.on('slideChange', function () {
+	document.getElementById("slide-number").textContent = `0${welcomeSlider.realIndex + 1}`;
 })
 
 
@@ -138,6 +139,45 @@ for (let i = 0; i < videoProgress.length; i++) {
 	})
 }
 
+
+//*! video-slider
+
+const videoSlider = new Swiper('.video-slider', {
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 30,
+
+	pagination: {
+		bulletClass: 'video-slider-bullet',
+		bulletActiveClass: 'video-slider-bullet-active',
+		el: '.video-slider-pagination',
+		clickable: '.true',
+	},
+
+	navigation: {
+		nextEl: '.video-slider-button-next',
+		prevEl: '.video-slider-button-prev',
+	},
+},
+);
+
+
+let videoSliderSlide = document.querySelectorAll('.video-slider-slide');
+
+const videoSliderSizing = () => {
+	videoSliderSlide.forEach(i => i.style.height = (i.offsetWidth * 0.561947) + 'px')
+};
+
+window.addEventListener('resize', () => videoSliderSizing());
+document.addEventListener('DOMContentLoaded', () => videoSliderSizing());
+
+// videoSliderSlide.forEach(i => {
+// 	i.onplay = (e) => { console.log(e) }
+// 	i.onpause = (e) => { console.log(e) }
+// });
+
+
+
 //*! gallery-randomize
 
 const galleryImgContainer = document.querySelector('.gallery-img-container');
@@ -180,56 +220,172 @@ const ticketSeniorAmount = document.querySelectorAll('.senior-amount');
 const ticketBasicSum = document.querySelector('.basic-sum');
 const ticketSeniorSum = document.querySelector('.senior-sum');
 const ticketTotalSum = document.querySelectorAll('.total-sum');
+const exhibitionTypeRadioButtons = document.querySelectorAll('.tickets-type-choise-input');
+const exhibitionTypeSelect = document.querySelector('.tickets-select');
+const ticketTimeInput = document.querySelector('.time')
+const ticketsDateInput = document.querySelector('.tickets-date')
+const ticketDateString = document.querySelector('.day')
+const exhibitionString = document.querySelector('.exhibition-string')
+const nameInput = document.querySelector('.input-name')
+const emailInput = document.querySelector('.input-email')
+const phoneInput = document.querySelector('.input-phone')
+const namePrompt = document.querySelector('.invalid-name')
+const emailPrompt = document.querySelector('.invalid-email')
+const phonePrompt = document.querySelector('.invalid-phone')
+const basicPriceString = document.querySelector('.basic-ticket-price');
+const seniorPriceString = document.querySelector('.senior-ticket-price');
+
+
 
 let counterBasic = 1;
 let counterSenior = 1;
+let exhibitionType = 1;
 
-ticketBasicAmount.forEach(item => item.textContent = counterBasic);
-ticketSeniorAmount.forEach(item => item.textContent = counterSenior);
-ticketBasicSum.textContent = `${counterBasic * 20}`;
-ticketSeniorSum.textContent = `${counterSenior * 10}`;
-ticketTotalSum.forEach(item => item.textContent = `${counterBasic * 20 + counterSenior * 10}`);
 
+const ticketsTime = document.querySelector('.tickets-time')
+let availableTime = [];
+for (let i = 9; i < 19; i++) {
+	availableTime.push(i + ' : 00', i + ' : 30');
+}
+availableTime.pop();
+for (let i = 0; i < availableTime.length; i++) availableTime[i] = `<option value="${availableTime[i]}">${availableTime[i]}</option>`
+ticketsTime.innerHTML = availableTime.join('');
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const exhibTypes = ['Permanent exhibition', 'Temporary exhibition', 'Combined Admission'];
+let today = new Date();
+
+let dd = today.getDate();
+let mm = today.getMonth() + 1;
+if (dd < 10) dd = '0' + dd
+if (mm < 10) mm = '0' + mm
+
+ticketsDateInput.setAttribute('min', today.getFullYear() + '-' + mm + '-' + dd);
+
+let date;
+const setDate = () => {
+	let dateString = daysOfWeek[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate();
+	ticketDateString.textContent = dateString;
+	localStorage.setItem('dateString', dateString);
+}
+
+
+const setAmountAndPrice = () => {
+
+	ticketBasicAmount.forEach(item => item.textContent = counterBasic);
+	ticketSeniorAmount.forEach(item => item.textContent = counterSenior);
+
+	let basicPrice = 20 * exhibitionType;
+	let seniorPrice = 10 * exhibitionType;
+	let basicPriceSum = counterBasic * basicPrice;
+	let seniorPriceSum = counterSenior * seniorPrice;
+	let totalPrice = basicPriceSum + seniorPriceSum;
+
+	ticketBasicSum.textContent = basicPriceSum;
+	ticketSeniorSum.textContent = seniorPriceSum;
+	basicPriceString.textContent = basicPrice;
+	seniorPriceString.textContent = seniorPrice;
+
+	switch (exhibitionType) {
+		case '1': exhibitionString.textContent = exhibTypes[0]; break;
+		case '1.25': exhibitionString.textContent = exhibTypes[1]; break;
+		case '2': exhibitionString.textContent = exhibTypes[2]; break;
+	}
+
+	exhibitionTypeSelect.value = exhibitionType;
+	exhibitionTypeRadioButtons.forEach(i => { if (i.value === exhibitionType) i.checked = true });
+
+	ticketTotalSum.forEach(item => item.textContent = totalPrice);
+
+	localStorage.setItem('exhibitionType', exhibitionType);
+	localStorage.setItem('counterBasic', counterBasic);
+	localStorage.setItem('counterSenior', counterSenior);
+
+}
+
+
+
+document.body.addEventListener('change', function (e) {
+	switch (e.target.name) {
+		case 'tickets-type-choise-input':
+			exhibitionType = e.target.value;
+			break;
+		case 'time':
+			ticketTimeInput.textContent = e.target.value;
+			localStorage.setItem('ticketTime', e.target.value);
+			break;
+		case 'date':
+			date = new Date(e.target.value);
+			setDate();
+			break;
+		case 'tickets-name':
+			namePrompt.style.display = e.target.validity.valid ? 'none' : 'block';
+			break;
+		case 'tickets-email':
+			emailPrompt.style.display = e.target.validity.valid ? 'none' : 'block';
+			break;
+		case 'tickets-phone':
+			phonePrompt.style.display = e.target.validity.valid ? 'none' : 'block';
+			break;
+	}
+	setAmountAndPrice();
+});
 
 for (let i = 0; i < ticketBasicMinus.length; i++) {
 	ticketBasicMinus[i].addEventListener("click", function () {
 		if (counterBasic > 0) {
-			counterBasic--
-			ticketBasicAmount.forEach(item => item.textContent = counterBasic);
-			ticketBasicSum.textContent = `${counterBasic * 20}`;
-			ticketTotalSum.forEach(item => item.textContent = `${counterBasic * 20 + counterSenior * 10}`);
+			counterBasic--;
+			setAmountAndPrice();
 		}
 	});
 
 	ticketBasicPlus[i].addEventListener("click", function () {
 		if (counterBasic < 20) {
 			counterBasic++;
-			ticketBasicAmount.forEach(item => item.textContent = counterBasic);
-			ticketBasicSum.textContent = `${counterBasic * 20}`;
-			ticketTotalSum.forEach(item => item.textContent = `${counterBasic * 20 + counterSenior * 10}`);
+			setAmountAndPrice();
 		}
 	});
 
 	ticketSeniorMinus[i].addEventListener("click", function () {
 		if (counterSenior > 0) {
 			counterSenior--
-			ticketSeniorAmount.forEach(item => item.textContent = counterSenior);
-			ticketSeniorSum.textContent = `${counterSenior * 10}`;
-			ticketTotalSum.forEach(item => item.textContent = `${counterBasic * 20 + counterSenior * 10}`);
+			setAmountAndPrice();
 		}
 	});
 
 	ticketSeniorPlus[i].addEventListener("click", function () {
 		if (counterSenior < 20) {
 			counterSenior++;
-			ticketSeniorAmount.forEach(item => item.textContent = counterSenior);
-			ticketSeniorSum.textContent = `${counterSenior * 10}`;
-			ticketTotalSum.forEach(item => item.textContent = `${counterBasic * 20 + counterSenior * 10}`);
+			setAmountAndPrice();
 		}
 	});
 }
 
-console.log('Предварительная оценка - 153 балла')
-console.log('Не выполненные/частично выполненные пункты:')
-console.log('-3 Для 420px и 768px не полностью сверстано меню;')
-console.log('-4 Результат проверки скорости сайта - средний')
+document.addEventListener('DOMContentLoaded', () => {
+	exhibitionType = localStorage.getItem('exhibitionType') || 1;
+	counterBasic = localStorage.getItem('counterBasic') || 1;
+	counterSenior = localStorage.getItem('counterSenior') || 1;
+	ticketTimeInput.textContent = localStorage.getItem('ticketTime') || '-- -- --';
+	ticketDateString.textContent = localStorage.getItem('dateString') || '-- -- --';
+	setAmountAndPrice();
+});
+
+
+//*! Contacts mapbox
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWx1YS1keiIsImEiOiJja3VtZ2U3NWIwZTY2Mm9waXp3anBwNGpuIn0.bUEBVKqZ5IAe5T1_vQSZAA';
+const map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/light-v10',
+	center: [2.3364123, 48.860915],
+	zoom: 16,
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+const marker1 = new mapboxgl.Marker({ color: 'black' }).setLngLat([2.3364, 48.86091]).addTo(map);
+const marker2 = new mapboxgl.Marker({ color: 'grey' }).setLngLat([2.3333, 48.8602]).addTo(map);
+const marker3 = new mapboxgl.Marker({ color: 'grey' }).setLngLat([2.3397, 48.8607]).addTo(map);
+const marker4 = new mapboxgl.Marker({ color: 'grey' }).setLngLat([2.3330, 48.8619]).addTo(map);
+const marker5 = new mapboxgl.Marker({ color: 'grey' }).setLngLat([2.3365, 48.8625]).addTo(map);
